7ee825a3730bf1a97b2f11a2356b75d2ec0aed17
Pableiro09 <prs0011@alu.medac.es>
2023-10-23 13:53:24 +0200

    tercera sesion

--- app/src/main/java/com/example/afuego/PageEdit.java
+++ app/src/main/java/com/example/afuego/PageEdit.java
@@ -2,13 +2,17 @@ package com.example.afuego;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PageEdit extends AppCompatActivity {
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,10 @@ public class PageEdit extends AppCompatActivity {
         setContentView(R.layout.page_edit);
 
         ImageView flechaedit = findViewById(R.id.flechaedit);
+        ImageView suma1 = findViewById(R.id.suma1);
+        ImageView imagen1 = findViewById(R.id.imagen1);
+
+
         flechaedit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -23,7 +31,34 @@ public class PageEdit extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
+        
+        suma1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir el explorador de archivos
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            // Obtener la URI de la imagen seleccionada
+            Uri imageUri = data.getData();
+
+            // Establecer la imagen en la ImageView
+            imagen1.setImageURI(imageUri);
+        }
+    }
+
 }
